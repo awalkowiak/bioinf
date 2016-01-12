b7909f2a378a6096e6155d13531317ea5f93ef66
awalkowiak <ar2rw92@gmail.com>
2016-01-12 19:00:26 +0100

    update

--- proj5.py
+++ proj5.py
@@ -1,24 +1,139 @@
 from Bio import AlignIO
-
+from Bio import motifs
 import os
+
 os.system('cls')
 x = 0
 
-def alignment(case):
-	opcje = {'stockholm':'file.sth', 'phylip':'file.phy'}
-	print opcje[case]
-	alignment = AlignIO.read(open(opcje[case]), case)
+def alignmentread():
+	alignment = AlignIO.read('file.phy', 'phylip')
+	tekst = ""
+	tekst += open('file.phy').read()
+	print "Dane z pliku:\n" + tekst + "\n\nWynik:\n"
+
 	print "Alignment length %i" % alignment.get_alignment_length()
 	for record in alignment :
 		print record.seq, record.id
 
+def alignmentparse():
+	#przyklad funkcji alignio.parse z rozdziału 6.1
+	pass
 
-while (x != '9'):
-	x = raw_input("1.Opcja stockholm.\n2.Opcja phylip.\n3.Opcja 3.\n0.Wyjscie\nWybierz co chcesz zrobic: ")
-	print "\nWybrales opcje: " + str(x)
-	cases = ['opcja0','stockholm', 'phylip']
-	
-	alignment(cases[int(x)])
-	
-	raw_input("Press Enter to continue...")
+def alignmentwrite():
+	#przyklad funkcji alignio.write z rozdziału 6.2
+	pass
+
+def alignmentslice():
+	#przyklad obrabiania dopasowań z rozdziału 6.3
+	pass
+
+def clustalw():
+	#przyklad narzedzia clustalW z rozdziału 6.4.1 + mozna walnac to drzewo zo jest pod koniec tego rozdzialu
+	pass
+
+def muscle():
+	#przyklad narzedzia MUSCLE z rozdziału 6.4.2
+	pass
+
+def emboss():
+	#przyklad narzedzia emboss z rozdziału 6.4.5
+	pass
+
+def creatingamotif():
+	# 14.1.1  Creating a motif from instances + 14.1.2  Creating a sequence logo
+	pass
+
+def jaspar():
+	# 14.2.1  JASPAR
+	pass
+
+def meme():
+	# 14.2.2  MEME
+	pass
+
+def transfac():
+	# 14.2.3  TRANSFAC
+	pass
+
+def writingmotifs():
+	#14.3  Writing motifs
+	pass
+
+def pwm():
+	# 14.4  Position-Weight Matrices
+	pass
+
+def pssm():
+	# 14.5  Position-Specific Scoring Matrices
+	pass
+
+def instances():
+	# 14.6  Searching for instances
+	pass
+
+def comparing():
+	# 14.8  Comparing motifs
+	pass
+
+def denovo():
+	# 14.9  De novo motif finding
+	pass
+
+def wyjscie():
+	print "Dobranoc!"
+
+while (x != '0'):
+	print "1. Bio.AlignIO\n2. Bio.Motifs\n"
+	x = raw_input(	"	11.alignmentread.\n" +
+					"	12.alignmentparse.\n" +
+					"	13.alignmentwrite.\n" +
+					"	14.alignmentslice.\n" +
+					"	15.clustalw.\n" +
+					"	16.muscle.\n" +
+					"	17.emboss.\n\n" +
+					"	21.creating a motif.\n" +
+					"	22.reading motifs (jaspar).\n" +
+					"	23.reading motifs (meme).\n" +
+					"	24.reading motifs (transfac).\n" +
+					"	25.writing motifs.\n" +
+					"	26.Position-Weight Matrices.\n" +
+					"	27.Position-Specific Scoring Matrices.\n" +
+					"	28.Instances.\n" +
+					"	29.Comparing.\n" +
+					"	210.de novo.\n\n" +
+
+					"0. Wyjscie\n\n" +
+					"Wybierz co chcesz zrobic: " )
+
+	cases = {	'11':alignmentread,
+				'12':alignmentparse,
+				'13':alignmentwrite,
+				'14':alignmentslice,
+				'15':clustalw,
+				'16':muscle,
+				'17':emboss,
+				'21':creatingamotif,
+				'22':jaspar,
+				'23':meme,
+				'24':transfac,
+				'25':writingmotifs,
+				'26':pwm,
+				'27':pssm,
+				'28':instances,
+				'29':comparing,
+				'210':denovo,
+
+				'0':wyjscie,
+				}
+	try:
+		os.system('cls')
+		print "\nWybrales opcje: " + str(x) + " " + str(cases[x])
+		if x != '0':
+			cases[x]()
+		else:
+			print "\nDobranoc!"
+	except:
+		print "\nzly znak"
+
+	raw_input("\n\nPress Enter to continue...")
 	os.system('cls')
